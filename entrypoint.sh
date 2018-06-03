bb6557bc67f950ac6a6d01f63ef5c961f27fb9ba
dreamren <mail@dream.ren>
2018-06-03 23:53:48 +0800

    Add files via upload

---
+++ entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+wget https://dream.ren/down/other/nginx_docker.conf -O /etc/nginx/nginx.conf --no-check-certificate
+
+/etc/init.d/ssh restart
+
+nginx -t
+
+nginx
+
+/etc/init.d/shadowsocks-libev start
+
+nohup kcptun -c /etc/kcptun.json &
+
+#wc
+tail -f /var/log/nginx/error.log > /dev/null
