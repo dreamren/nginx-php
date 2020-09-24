b494a1e3b2e3e92d57c799733070a0d7e93f62ad
dreamren <mail@dream.ren>
2020-09-25 00:39:32 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,28 +2,12 @@
 
 cp -f /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
 
-#wget https://dream.ren/down/other/nginx_docker.conf -O /etc/nginx/nginx.conf --no-check-certificate
-
 /etc/init.d/ssh restart
-
-#nginx -t
-
-#nginx
-
-/etc/init.d/shadowsocks-libev start
-
-
-nohup kcptun -c /etc/kcptun-ss.json &
 
 while :
 do
-  fi
-  netstat -unlp |grep 999 > /dev/null
-  if [ $? -ne 0 ];then
-    nohup kcptun -c /etc/kcptun-ss.json &
-  fi
-  sleep 10
+  sleep 10000
 done
 
 #wc
-#tail -f /var/log/nginx/error.log > /dev/null
+tail -f /usr/share/zoneinfo/Asia/Shanghai > /dev/null
